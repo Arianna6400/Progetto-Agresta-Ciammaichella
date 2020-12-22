bb511d3c6ff260d90128055ff1f232f0dad5eb13
Arianna <74916702+Arianna6400@users.noreply.github.com>
2020-12-22 18:11:08 +0100

    Update LessThan.java

--- ProgettoOOP/src/main/java/com/example/ProgettoOOP/Filters/LessThan.java
+++ ProgettoOOP/src/main/java/com/example/ProgettoOOP/Filters/LessThan.java
@@ -5,20 +5,15 @@ import java.util.List;
 import java.util.Vector;
 
 import com.example.ProgettoOOP.Types.UVData;
-import com.example.ProgettoOOP.util.Reader;
-import com.google.gson.Gson;
 
 public class LessThan {
-	public static String getLess (double num) throws IOException {
-		Vector<UVData> fin = new Vector<UVData>();
-		List<UVData> data = Reader.getVectorFile();
-		for(UVData d : data) {
+	public static List<UVData> getLess (List<UVData> DataSet, double num) throws IOException {
+		List<UVData> fin = new Vector<UVData>();
+		for(UVData d : DataSet) {
 			if(d.value<num) {
 				fin.add(d);
 			}
 		}
-		Gson gson= new Gson();
-		String filteredJson = gson.toJson(fin);
-		return filteredJson;
+		return fin;
 	}
 }
