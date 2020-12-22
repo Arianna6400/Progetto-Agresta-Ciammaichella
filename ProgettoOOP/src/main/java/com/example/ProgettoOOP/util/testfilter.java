71a8646453dee7d70fcf6015ace1f6d2630181ce
MarcoCiammaichella <74916759+MarcoCiammaichella@users.noreply.github.com>
2020-12-22 16:45:15 +0100

    Update testfilter.java

--- ProgettoOOP/src/main/java/com/example/ProgettoOOP/util/testfilter.java
+++ ProgettoOOP/src/main/java/com/example/ProgettoOOP/util/testfilter.java
@@ -5,19 +5,35 @@ import com.example.ProgettoOOP.Filters.*;
 import com.example.ProgettoOOP.Types.*;
 
 public class testfilter {
-	public static String getFilter(Value value) throws IOException {
-		if(value.Greater != 0) {
-			return (GreaterThan.getGreater(value.Greater));
+	public static String getFilter(test parent) throws IOException {
+		if(parent.Value != null) {
+			if(parent.Value.Greater != 0) {
+				return (GreaterThan.getGreater(parent.Value.Greater));
 			}
-		if(value.Less != 0) {
-			return(LessThan.getLess(value.Less));
+			if(parent.Value.Less != 0) {
+				return(LessThan.getLess(parent.Value.Less));
 			}
-		if(value.Included != null  ) {
-			return(Included.getIncluded(value.Included.get(0),value.Included.get(1)));
+			if(parent.Value.Included != null  ) {
+				return(Included.getIncluded(parent.Value.Included.get(0),parent.Value.Included.get(1)));
 			}
-		if(value.NotIncluded!= null ) {
-			return(NotIncluded.getNotIncluded(value.NotIncluded.get(0),value.NotIncluded.get(1)));
+			if(parent.Value.NotIncluded!= null ) {
+				return(NotIncluded.getNotIncluded(parent.Value.NotIncluded.get(0),parent.Value.NotIncluded.get(1)));
 			}
+		}
+		if(parent.Date!= null) {
+			if(parent.Date.Greater != 0) {
+				return (GreaterThan.getGreater(parent.Date.Greater));
+			}
+			if(parent.Date.Less != 0) {
+				return(LessThan.getLess(parent.Date.Less));
+			}
+			if(parent.Date.Included != null  ) {
+				return(Included.getIncluded(parent.Date.Included.get(0),parent.Date.Included.get(1)));
+			}
+			if(parent.Date.NotIncluded!= null ) {
+				return(NotIncluded.getNotIncluded(parent.Date.NotIncluded.get(0),parent.Date.NotIncluded.get(1)));
+			}
+		}
 		return "not found";
 	}
 }
