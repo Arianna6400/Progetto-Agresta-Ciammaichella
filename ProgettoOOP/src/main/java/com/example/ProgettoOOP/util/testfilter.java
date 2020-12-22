baa7feccdf7d1cfdd22e6e06be9df24aa5580540
MarcoCiammaichella <74916759+MarcoCiammaichella@users.noreply.github.com>
2020-12-22 18:10:17 +0100

    Update testfilter.java

--- ProgettoOOP/src/main/java/com/example/ProgettoOOP/util/testfilter.java
+++ ProgettoOOP/src/main/java/com/example/ProgettoOOP/util/testfilter.java
@@ -3,35 +3,37 @@ package com.example.ProgettoOOP.util;
 import java.io.IOException;
 import com.example.ProgettoOOP.Filters.*;
 import com.example.ProgettoOOP.Types.*;
+import java.util.List;
 
 public class testfilter {
 	public static String getFilter(test parent) throws IOException {
+		List<UVData> DataSet=Reader.getVectorFile();
 		if(parent.Value != null) {
 			if(parent.Value.Greater != 0) {
-				return (GreaterThan.getGreater(parent.Value.Greater));
+				DataSet = GreaterThan.getGreater(DataSet,parent.Value.Greater);
 			}
 			if(parent.Value.Less != 0) {
-				return(LessThan.getLess(parent.Value.Less));
+				DataSet=LessThan.getLess(DataSet,parent.Value.Less);
 			}
 			if(parent.Value.Included != null  ) {
-				return(Included.getIncluded(parent.Value.Included.get(0),parent.Value.Included.get(1)));
+				DataSet=Included.getIncluded(DataSet,parent.Value.Included.get(0),parent.Value.Included.get(1));
 			}
 			if(parent.Value.NotIncluded!= null ) {
-				return(NotIncluded.getNotIncluded(parent.Value.NotIncluded.get(0),parent.Value.NotIncluded.get(1)));
+				DataSet=NotIncluded.getNotIncluded(DataSet,parent.Value.NotIncluded.get(0),parent.Value.NotIncluded.get(1));
 			}
 		}
 		if(parent.Date!= null) {
 			if(parent.Date.Greater != 0) {
-				return (GreaterThan.getGreater(parent.Date.Greater));
+				DataSet=GreaterThan.getGreater(DataSet,parent.Date.Greater);
 			}
 			if(parent.Date.Less != 0) {
-				return(LessThan.getLess(parent.Date.Less));
+				DataSet=LessThan.getLess(DataSet,parent.Date.Less);
 			}
 			if(parent.Date.Included != null  ) {
-				return(Included.getIncluded(parent.Date.Included.get(0),parent.Date.Included.get(1)));
+				DataSet=Included.getIncluded(DataSet,parent.Date.Included.get(0),parent.Date.Included.get(1));
 			}
 			if(parent.Date.NotIncluded!= null ) {
-				return(NotIncluded.getNotIncluded(parent.Date.NotIncluded.get(0),parent.Date.NotIncluded.get(1)));
+				DataSet=NotIncluded.getNotIncluded(DataSet,parent.Date.NotIncluded.get(0),parent.Date.NotIncluded.get(1));
 			}
 		}
 		return "not found";
